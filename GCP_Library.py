f2d93b86161ae1c15cb90d679339bee22dcdf428
Daniel Lavedonio de Lima <daniel.lavedonio@gmail.com>
2019-12-28 03:50:14 -0300

    Added error handling support

--- GCP_Library.py
+++ GCP_Library.py
@@ -25,6 +25,11 @@ class GCPTool(object):
         # Code created following Google official API documentation:
         # https://cloud.google.com/bigquery/docs/reference/libraries
 
+        # Checking if no client is instantiated
+        if len(init_list) == 0:
+            logger.error("ERROR: No client instantiated on 'init_list' class initialization parameter")
+            raise ValueError("No client instantiated on 'init_list' class initialization parameter")
+
         # Getting credential files path
         try:
             credentials_path = os.environ["CREDENTIALS_HOME"]
@@ -35,17 +40,40 @@ class GCPTool(object):
         finally:
             os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join(credentials_path, connect_file)
 
+        # Default values
         bq_client = None
 
         if "bigquery" in init_list:
-            bq_client = bigquery.Client()
+            logger.debug("Initiating BigQuery Client")
+            
+            try:
+                bq_client = bigquery.Client()
+                logger.debug("Connected.")
+            
+            except Exception as e:
+                logger.exception("Error connecting with BigQuery!")
+                raise e
 
         self.bq_client = bq_client
 
     def query(self, sql_query):
         """Run a query and return the results as a Pandas Dataframe"""
 
-        return self.bq_client.query(sql_query).to_dataframe()
+        logger.debug(f"Initiating query: {sql_query}")
+        try:
+            result = self.bq_client.query(sql_query).to_dataframe()
+            logger.debug("Query returned successfully.")
+
+        except AttributeError as ae:
+            logger.exception("BigQuery client not initialized")
+            print("\n------\nERROR: BigQuery client not initialized\n------\n")
+            raise ae
+
+        except Exception as e:
+            logger.exception("Query failed")
+            raise e
+
+        return result
 
 
 def test():
@@ -53,7 +81,8 @@ def test():
 
     sql_test_query = """SELECT MAX(etl_tstamp) FROM `revelo-hebe.source_fausto.atomic_events`"""
 
-    bq.query(sql_test_query)
+    df = bq.query(sql_test_query)
+    print(df)
 
 
 if __name__ == '__main__':
