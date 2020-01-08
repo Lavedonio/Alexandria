760ff263020f9c3124ea6776ef0e1487c05eff64
Daniel Lavedonio de Lima <daniel.lavedonio@gmail.com>
2020-01-08 02:47:10 -0300

    Added missing arguments: install_requires, license and platforms. Package name changed so it's unique. New version released with changes (0.0.2).

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="alexandria",
-    version="0.0.1",
+    name="alexandria-lavedonio",
+    version="0.0.2",
     author="Daniel Lavedonio de Lima",
     author_email="daniel.lavedonio@gmail.com",
     description="A package to help with DB connections and commonly used functionally in data analytics",
@@ -13,10 +13,19 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Lavedonio/alexandria",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'boto3',
+        'google-cloud-bigquery',
+        'google-cloud-storage',
+        'pandas',
+        'psycopg2',
+    ],
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    platforms=['any'],
 )
