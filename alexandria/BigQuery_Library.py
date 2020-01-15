1699f3ebea0879c3c54732a30ed4a06226e8cc04
Daniel Lavedonio de Lima <daniel.lavedonio@gmail.com>
2020-01-15 02:05:54 -0300

    Added project_name and transfer_name parameters for start_transfer method as a second option for retrieving the project_path in a more readable way.

--- alexandria/BigQuery_Library.py
+++ alexandria/BigQuery_Library.py
@@ -84,11 +84,48 @@ class BigQueryTool(object):
         destination = dataset + "." + table
         dataframe.to_gbq(destination, chunksize=None, if_exists=if_exists)
 
-    def start_transfer(self, project_path):
-        """Trigger a transfer to start executing in BigQuery Transfer."""
+    def start_transfer(self, project_path=None, project_name=None, transfer_name=None):
+        """Trigger a transfer to start executing in BigQuery Transfer.
+        API documentation: https://googleapis.dev/python/bigquerydatatransfer/latest/gapic/v1/api.html
+        """
+
+        # Getting dictionary of project names and ids.
+        project_ids = fetch_credentials("BigQuery", dictionary="project_id")
 
         # Initiating client
         self.transfer_client = bigquery_datatransfer_v1.DataTransferServiceClient()
+
+        # If project_path is given with other parameter, it will ignore the others and continue with
+        # project_path given.
+        if project_path is None:
+            # If one of the arguments is missing, this method fails
+            if project_name is None or transfer_name is None:
+                logger.exception("Specify either project and transfer names or transferConfig project path.")
+                raise ValueError("Specify either project and transfer names or transferConfig project path.")
+
+            else:
+                # Get project id from dictionary
+                try:
+                    project_id = project_ids[project_name]
+                except KeyError:
+                    logger.exception("Project name not found in secrets file. Please add in secrets file or use the project_path parameter instead.")
+                    raise KeyError("Project name not found in secrets file. Please add in secrets file or use the project_path parameter instead.")
+
+                # Setting parent project path
+                parent = self.transfer_client.project_path(project_id)
+
+                # Listing all transfers and retrieving transfer_id from match with transfer display_name
+                for element in self.transfer_client.list_transfer_configs(parent):
+                    if element.display_name == transfer_name:
+                        transfer_id = element.name.split("/")[-1]
+                        break
+
+                # Setting project path. If there was no match for transfer, this method fails
+                try:
+                    project_path = self.transfer_client.project_transfer_config_path(project_id, transfer_id)
+                except NameError:
+                    logger.exception("No transfer with display name given was found.")
+                    raise NameError("No transfer with display name given was found.")
 
         # Getting current timestamp so it starts now.
         # Google documentation: https://developers.google.com/protocol-buffers/docs/reference/csharp/class/google/protobuf/well-known-types/timestamp
@@ -142,6 +179,7 @@ def test():
     transfer_config = "projects/118480078918/transferConfigs/5e35261a-0000-2d5a-bd96-24058872d28c"
     print("Starting transfer...")
     state_response = bq.start_transfer(project_path=transfer_config)
+    state_response = bq.start_transfer(project_name="revelo-hebe", transfer_name="atomic_events_test3")
     print(f"Transfer status: {state_response}")
 
 
