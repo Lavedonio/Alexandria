85e91d045df3609d249def6fe71d3746c935c551
Daniel Lavedonio de Lima <daniel.lavedonio@gmail.com>
2020-01-27 23:55:10 -0300

    Version 0.0.1 (alpha) released!!! Also small fixes.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     version="0.0.1",
     author="Daniel Lavedonio de Lima",
     author_email="daniel.lavedonio@gmail.com",
-    description="A package to help with DB connections and commonly used functionally in data analytics",
+    description="A package to help with DB connections and commonly used functionalities in data analytics",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Lavedonio/instackup",
