379faff594360b756486b94a9d2dc11453b855e6
Daniel Lavedonio de Lima <daniel.lavedonio@gmail.com>
2020-04-02 09:55:14 -0300

    Updated testing

--- test/test_gcloudstorage_tools.py
+++ test/test_gcloudstorage_tools.py
@@ -2,38 +2,34 @@ from instackup.gcloudstorage_tools import GCloudStorageTool
 
 
 def test():
-    cloud_storage = GCloudStorageTool()
+    # cloud_storage = GCloudStorageTool()
 
-    gs_path = "gs://revelo_app_etl/"
+    gs_path = "gs://revelo-rais/"
 
     cloud_storage = GCloudStorageTool(gs_path=gs_path)
-    gs_path = "gs://revelo_app_etl/revelo-app/"
+    print(cloud_storage.get_bucket_info())
+    gs_path = "gs://revelo-rais/RAIS_2014/"
     cloud_storage.set_by_path(gs_path)
 
+    cloud_storage.set_blob("gs://revelo-rais/RAIS_2014/AC2014.txt")
+    print(cloud_storage.get_blob_info())
+
     buckets = cloud_storage.list_all_buckets()
-    print("Bucket list:")
+    print("\nBucket list:")
     for index, bucket in enumerate(buckets):
-        print(f"{index}: {bucket}")
+        print(f"{index}: {bucket['Name']}")
 
-    contents = cloud_storage.list_bucket_contents()
+    contents = cloud_storage.list_contents()
 
-    print("File list:")
+    print("\nFile list:")
     for index, content in enumerate(contents):
-        print(f"{index}: {content}")
+        print(f"{index}: {content['Name']}")
 
-    # file = "C:\\Users\\USER\\Downloads\\teste.txt"
-    # new_file = "teste.txt"
-    # remote = "teste/teste.txt"
+    # filename = "C:\\Users\\USER\\Downloads\\RAIS_2014\\AC2014.txt"
 
-    # cloud_storage.upload_file(file)
+    # cloud_storage.download_file(filename)
 
-    # contents = cloud_storage.list_bucket_contents()
-
-    # print("\nUpdated file list:")
-    # for index, content in enumerate(contents):
-    #     print(f"{index}: {content}")
-
-    # cloud_storage.download_file(remote, new_file)
+    print(cloud_storage.download_on_dataframe(sep=";", encoding="latin1", decimal=","))
 
 
 if __name__ == '__main__':
