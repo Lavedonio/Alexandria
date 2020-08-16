2ebd601915ef929c4f75600cde3f3f167529e582
Daniel Lavedonio de Lima <daniel.lavedonio@gmail.com>
2020-08-16 14:56:14 -0300

    Releasing quick bug fix

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="instackup",
-    version="0.1.0",
+    version="0.1.1",
     author="Daniel Lavedonio de Lima",
     author_email="daniel.lavedonio@gmail.com",
     description="A package to ease interaction with cloud services, DB connections and commonly used functionalities in data analytics.",
